575a49fe2f6077483408d5d54df7d622c1d5e99b
Muhammad Shafeen <112253449+shafeenyousafzaii@users.noreply.github.com>
2025-07-10 20:17:05 +0500

    Some minor changes

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template,request
+from socket import gethostname
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     return "Hello World!"
-@app.route('/index')
+@app.route('/index',methods=['GET'])
 def index():
-    return('template/index.html')
+    return render_template('index.html')
+if __name__ == '__main__':
+        app.run(debug=True)
